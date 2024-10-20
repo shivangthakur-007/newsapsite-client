b2cefa6f6a3967ea1e8e08f5ec32cd6eb9c91997
Dance <thakurshivang579@gmail.com>
2024-10-20 10:25:02 +0530

    adding all files seperate from grocery

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import Readnews from "./Readnews/Readnews";
 import Category from "./Readnews/Dashboard/Category";
 import NotFound from "./Homepage/NotFound";
-import Grocery from "./Readnews/Dashboard/dailygrocerystore/Grocery";
-import Grocerytemplate from "./Readnews/Dashboard/Category/Grocerytemplate";
-import Grocerytrack from "./Readnews/Dashboard/dailygrocerystore/TrackingOrder/Grocerytrack";
-import GroceryCheckout from "./Readnews/Dashboard/dailygrocerystore/Checkout/groceryCheckout";
 
 const queryClient = new QueryClient();
 
@@ -19,10 +15,6 @@ function App() {
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/read" element={<Readnews />}></Route>
         <Route path={"/read/:useparam"} element={<Category />}></Route>
-        <Route path="/grocery" element={<Grocery />}></Route>
-        <Route path="/grocerytemplate" element={<Grocerytemplate />}></Route>
-        <Route path="/grocery/order-tracking" element={<Grocerytrack />}></Route>
-        <Route path="/grocery/checkout" element={<GroceryCheckout />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </QueryClientProvider>
